57225f39b6e86212f65f4d1c71ccfd7f90f86cb2
Cuongisreal <cuongnguyen20102001@gmail.com>
2024-08-10 17:11:52 +0700

    Update displayHeader.js

--- src/displayHeader.js
+++ src/displayHeader.js
@@ -4,8 +4,8 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log(colors.cyan('========================================'));
   console.log(colors.cyan('=      Units Network Testnet BOT       ='));
-  console.log(colors.cyan('=     Created by HappyCuanAirdrop      ='));
-  console.log(colors.cyan('=    https://t.me/HappyCuanAirdrop     ='));
+  console.log(colors.cyan('=     Created by Cuongisreal      ='));
+  console.log(colors.cyan('=    https://t.me/alpha_prompt     ='));
   console.log(colors.cyan('========================================'));
   console.log();
 }
